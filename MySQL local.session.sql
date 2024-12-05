b8f707d0d5288e188d6bdfae8d354f5820c4b2c8
tejalkavatage <tejalkk095@gmail.com>
2024-12-05 07:57:17 +0530

    php files added

---
+++ MySQL local.session.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Tejal123';
+
+SELECT * FROM users;
